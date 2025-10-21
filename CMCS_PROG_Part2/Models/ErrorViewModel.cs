d4fc781d49e53b9c16be3cf181edfa34b4b8c9f3
Sajana21 <ST10433940@vcconnect.edu.za>
2025-10-21 10:23:32 +0200

    Add project files.

---
+++ CMCS_PROG_Part2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMCS_PROG_Part2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
